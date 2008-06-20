8ec0380e255645801a7ac0da066fd73edcb18edf
zabbie <info@example.com>
2008-06-20 15:21:10 +0000

    load yahoo-dom-event.js
    git-svn-id: http://svn.projects.ez.no/accessibility_improvements@3 b3ab3c38-4f3d-40b7-b9e1-a77484a74240

--- trunk/settings/design.ini.append.php
+++ trunk/settings/design.ini.append.php
@@ -5,6 +5,7 @@ DesignExtensions[]=accessibility_improvements
 
 [JavaScriptSettings]
 JavaScriptList[]=editcheck.js
+JavaScriptList[]=yui/build/yahoo-dom-event/yahoo-dom-event.js
 
 [StylesheetSettings]
 CSSFileList[]=editcheck.css
